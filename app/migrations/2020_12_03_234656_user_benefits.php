03ad8f917d8f70ca7178ae9876211cce92de6bc7
Ebuka <ebukaodini@gmail.com>
2020-12-04 17:23:54 +0100

    migration files

--- app/migrations/2020_12_03_234656_user_benefits.php
+++ app/migrations/2020_12_03_234656_user_benefits.php
@@ -4,29 +4,11 @@ use Library\Database\Schema;
 
 function migrate()
 {
+   // this is where the user's accrued benefit should come to
    Schema::create('user_benefits', function(Schema $schema) {
       $schema->int('id')->auto_increment()->primary();
-      $schema->timestamp('created_at')->attribute();
+      $schema->varchar('status', 10)->default('');
+      $schema->timestamp('created_at');
       $schema->datetime('updated_at')->attribute("ON UPDATE CURRENT_TIMESTAMP");
    }, false, 'user_benefits');
-
-   // Schema::seed('user_benefits', 
-   //    [
-   //       'field' => 'value',
-   //       'field' => 'value',
-   //    ],
-   //    [
-   //       'field' => 'value',
-   //       'field' => 'value',
-   //    ],
-   //    ...
-   // );
-
-   // Schema::alter('user_benefits', function(Schema $schema) {
-   //    $schema->change('id')->double('id');
-   //    $schema->change('created_at')->datetime('created_at');
-   //    $schema->change('updated_at')->datetime('updated_at');
-   // }, false);
-
-   // Schema::drop('user_benefits');
 }
